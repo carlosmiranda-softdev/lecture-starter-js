a045263020740e10c8c0c28b39ba778802e6e187
Carlos Miranda <mirandadelahozcarlos@gmail.com>
2023-12-14 21:28:15 -0500

    + Added getRandom, getDamage, getHitPower and getBlockPower functions in fight file

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -1,19 +1,58 @@
 import controls from '../../constants/controls';
 
-export async function fight(firstFighter, secondFighter) {
-    return new Promise(resolve => {
-        // resolve the promise with the winner when fight is over
-    });
-}
-
-export function getDamage(attacker, defender) {
-    // return damage
+function getRandom(min, max) {
+    return Math.random() * (max - min) + min;
 }
 
 export function getHitPower(fighter) {
     // return hit power
+    const hitPower = fighter.attack * getRandom(1, 2);
+    return hitPower;
 }
 
 export function getBlockPower(fighter) {
     // return block power
+    const blockPower = fighter.defense * getRandom(1, 2);
+    return blockPower;
+}
+
+export function getDamage(attacker, defender) {
+    // return damage
+    const hitPowerAttacker = getHitPower(attacker);
+    const blockPowerDefender = getBlockPower(defender);
+    const damage = hitPowerAttacker - blockPowerDefender;
+
+    if (blockPowerDefender > hitPowerAttacker) {
+        return 0;
+    }
+    return damage;
+}
+
+export async function fight(firstFighter, secondFighter) {
+    let firstKey = null;
+    let secondKey = null;
+    const keysPressed = [];
+    document.addEventListener('keydown', event => {
+        if (
+            event.key === controls.PlayerOneAttack ||
+            event.key === controls.PlayerTwoAttack ||
+            event.key === controls.PlayerOneBlock ||
+            event.key === controls.PlayerTwoBlock
+        ) {
+            if (firstKey === null) {
+                firstKey = event.key;
+                keysPressed.push(firstKey);
+            } else if (firstKey !== null) {
+                secondKey = event.key;
+                keysPressed.push(secondKey);
+            }
+        }
+    });
+
+    getDamage(firstFighter, secondFighter);
+
+    return new Promise(resolve => {
+        // resolve the promise with the winner when fight is over
+        resolve();
+    });
 }
